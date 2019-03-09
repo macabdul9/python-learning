feb6a5b6ecbea2eb9286228d48b5a7239a8b8c48
macabdul9 <abdulwaheed1513@gmail.com>
2019-03-09 23:45:15 +0530

    pycharm commit

---
+++ operators.py
@@ -0,0 +1,12 @@
+
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+
+# arithmetic operator
+# print("a +- bi = ", a + b)
+# print("a / b = ", a / b, "a//b = ", a//b)
+
+# shift operator
+print (a << 1, a >> 1)
+
