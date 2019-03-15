0d588642774acec573cc411fe267aab039f2fbab
macabdul9 <abdulwaheed1513@gmail.com>
2019-03-15 22:51:24 +0530

    via terminal

---
+++ 2dlist.py
@@ -0,0 +1,11 @@
+"""
+ @author    : macab (macab@debian)
+ @file      : 2dlist
+ @created   : Friday Mar 15, 2019 22:32:09 IST
+"""
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+for row in matrix:
+    print(row)
+
