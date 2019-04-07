b851298994ee7fedbe52d9dd0c4a6002985599a7
macabdul9 <abdulwaheed1513@gmail.com>
2019-04-07 22:46:01 +0530

    via terminal

--- What'sApp/spamming.py
+++ What'sApp/spamming.py
@@ -7,25 +7,28 @@ import time
 
 # Replace below path with the absolute path 
 # to chromedriver in your computer 
-driver = webdriver.Chrome('/home/saket/Downloads/chromedriver')
+driver = webdriver.Chrome('/home/macab/chromedriver')
 
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
 
 # Replace 'Friend's Name' with the name of your friend  
 # or the name of a group  
-target = '"Friend\'s Name"'
+target = '"Gaurav MAIT"'
 
 # Replace the below string with your own message 
 string = "Message sent using Python!!!"
 
 x_arg = '//span[contains(@title,' + target + ')]'
+
 group_title = wait.until(EC.presence_of_element_located((
     By.XPATH, x_arg)))
 group_title.click()
+
 inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
-input_box = wait.until(EC.presence_of_element_located((
-    By.XPATH, inp_xpath)))
+
+input_box = wait.until(EC.presence_of_element_located((By.XPATH, inp_xpath)))
+
 for i in range(100):
     input_box.send_keys(string + Keys.ENTER)
     time.sleep(1) 
