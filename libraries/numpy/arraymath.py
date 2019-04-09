48060af542ddb99c4ca3cb89068ad636b5f6c35a
macabdul9 <abdulwaheed1513@gmail.com>
2019-04-09 23:39:48 +0530

    add files via terminal

---
+++ libraries/numpy/arraymath.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[11]:
+
+
+# matrix addition
+mat_1 = np.array([[1, 2], [2, 4]])
+mat_2 = np.array(([3, 5], [5, 7]))
+mat_add = mat_1 + mat_2
+print(np.add(mat_1, mat_2))
+print(mat_3)
+
+
+# In[7]:
+
+
+# subtraction
+mat_sub = mat_1 - mat_2
+print(mat_sub)
+
+
+# In[12]:
+
+
+# multiplication
+mat_mul = mat_1*mat_2
+print(mat_mul)
+print(np.multiply(mat_1, mat_2))
+
+
+# In[13]:
+
+
+# divide
+mat_div = mat_1/mat_2
+print(mat_div)
+print(np.divide(mat_1, mat_2))
+
+
+# In[16]:
+
+
+# square root of a matrix
+mat_sqrt = np.sqrt(mat_1)
+print(mat_sqrt)
+
+
+# In[ ]:
+
+
+
+
