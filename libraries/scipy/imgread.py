0954d81830ebeb061f0269bdc121f242dfd919a0
macabdul9 <abdulwaheed1513@gmail.com>
2019-04-10 23:15:05 +0530

    pycharm commit

--- libraries/scipy/imgread.py
+++ libraries/scipy/imgread.py
@@ -1,6 +1,39 @@
-import scipy.misc as sp
-import matplotlib.pyplot as plt
-img = sp.imread('cat.jpg')
-#sp.imshow(img)
-img = sp.imrotate(img, angle=90)
-sp.imshow(img)
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[22]:
+
+
+from scipy.misc import imread, imsave, imresize, imshow
+
+
+# In[23]:
+
+img = imread('cat.jpg')
+
+
+# In[24]:
+
+
+print(img.dtype, img.shape)
+#print(img)
+
+
+# In[20]:
+
+
+# image tinting 
+img_tinted = img * [1, 0.95, 0.9]
+
+
+# In[28]:
+
+
+print(imshow(img))
+
+
+# In[ ]:
+
+
+
+
