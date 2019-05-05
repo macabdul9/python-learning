50213ebcfef1fe82773bbab0892641e6ead7aeeb
macabdul9 <abdulwaheed1513@gmail.com>
2019-05-05 22:19:44 +0530

    add files via terminal

---
+++ libraries/numpy/uniform.py
@@ -0,0 +1,12 @@
+"""
+ @author    : macab (macab@debian)
+ @file      : uniform
+ @created   : Sunday May 05, 2019 22:17:01 IST
+"""
+
+import numpy as np
+
+x = np.random.uniform(0, 1, size = 100)
+
+print(x.mean())
+
